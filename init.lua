39ec919d3f8ae0433d321a12f16939442e6a559d
Mihailo Kaplarevic <m.kaplarevic@threedium.co.uk>
2024-04-19 16:04:03 +0200

    Disabled space, added keymaps for word wrap, added keymaps for splits, added keymaps for pasting and yanking to clipboard

--- init.lua
+++ init.lua
@@ -83,6 +83,19 @@ vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+-- Disable space(leader) key in normal and visual mode
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+vim.keymap.set('n', '<leader>|', vim.cmd.vsplit, { desc = 'Vertical split' })
+vim.keymap.set('n', '<leader>-', vim.cmd.split, { desc = 'Horizontal split' })
+
+vim.keymap.set('n', '<leader>p', [["+p]])
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
